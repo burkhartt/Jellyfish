619830d369c96bfde74e93f84268763a2116c3f9
Tim Burkhart <timmyb_84@hotmail.com>
2013-01-05 22:41:04 -0600

    Globally set the layout

---
+++ Web/Web/Config/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Config {
+    public abstract class BasePage : WebViewPage {
+        
+    }
+
+    public abstract class BasePage<T> : WebViewPage<T> {
+        
+
+        
+    }
+}
